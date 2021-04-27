c16b9a9a87f9d91a5ba73d93f860e2ec805d9b43
agar91 <agnieszkarapa@gmail.com>
2021-04-27 15:55:46 +0200

    Authors attached to posts

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -43,11 +43,13 @@ public class DatabaseLoader implements ApplicationRunner {
         IntStream.range(0,40).forEach(i->{
             String template = templates[i % templates.length];
             String gadget = gadgets[i % gadgets.length];
-
+            Author author = authors.get(i % authors.size());
             String title = String.format(template, gadget);
             Post post = new Post(title, "Lorem ipsum dolor sit amet, consectetur adipiscing elit… ");
             randomPosts.add(post);
+            post.setAuthor(author);
         });
         postRepository.saveAll(randomPosts);
+        authorRepository.saveAll(authors);
     }
 }
